da9e4d54aead0626f9cd6f8ca62c1899b1c8ddec
nulldoot2k <companydatv412@gmail.com>
2024-10-20 09:30:41 +0700

    Feat: Update Plugins, change type Text to URL

--- src/module.ts
+++ src/module.ts
@@ -1,40 +1,45 @@
 import { PanelPlugin } from '@grafana/data';
-import { SimpleOptions } from './types';
-import { SimplePanel } from './components/SimplePanel';
+// import { SimpleOptions } from './types';
+// import { SimplePanel } from './components/SimplePanel';
+import { PanelOptions } from './types';
+import { ImagePanel } from './components';
 
-export const plugin = new PanelPlugin<SimpleOptions>(SimplePanel).setPanelOptions((builder) => {
+/*
+  * Panel Plugins
+*/
+export const plugin = new PanelPlugin<PanelOptions>(ImagePanel).setPanelOptions((builder) => {
   return builder
     .addTextInput({
-      path: 'text',
-      name: 'Simple text option',
-      description: 'Description of panel option',
-      defaultValue: 'Default value of text input option',
+      path: 'url',
+      name: 'Image URL',
+      // description: 'Description of panel option',
+      defaultValue: 'https',
     })
-    .addBooleanSwitch({
-      path: 'showSeriesCount',
-      name: 'Show series counter',
-      defaultValue: false,
-    })
-    .addRadio({
-      path: 'seriesCountSize',
-      defaultValue: 'sm',
-      name: 'Series counter size',
-      settings: {
-        options: [
-          {
-            value: 'sm',
-            label: 'Small',
-          },
-          {
-            value: 'md',
-            label: 'Medium',
-          },
-          {
-            value: 'lg',
-            label: 'Large',
-          },
-        ],
-      },
-      showIf: (config) => config.showSeriesCount,
-    });
+    // .addBooleanSwitch({
+    //   path: 'showSeriesCount',
+    //   name: 'Show series counter',
+    //   defaultValue: false,
+    // })
+    // .addRadio({
+    //   path: 'seriesCountSize',
+    //   defaultValue: 'sm',
+    //   name: 'Series counter size',
+    //   settings: {
+    //     options: [
+    //       {
+    //         value: 'sm',
+    //         label: 'Small',
+    //       },
+    //       {
+    //         value: 'md',
+    //         label: 'Medium',
+    //       },
+    //       {
+    //         value: 'lg',
+    //         label: 'Large',
+    //       },
+    //     ],
+    //   },
+    //   showIf: (config) => config.showSeriesCount,
+    // });
 });
--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,18 @@
-type SeriesSize = 'sm' | 'md' | 'lg';
+// type SeriesSize = 'sm' | 'md' | 'lg';
 
-export interface SimpleOptions {
-  text: string;
-  showSeriesCount: boolean;
-  seriesCountSize: SeriesSize;
+// export interface SimpleOptions {
+//   text: string;
+//   showSeriesCount: boolean;
+//   seriesCountSize: SeriesSize;
+// }
+/**
+  * Panel Options
+*/
+export interface PanelOptions {
+  /**
+    * Image URL
+    *
+    * @type {string}
+  */
+  url: string;
 }
